aba3caaf48af6736d98e65f11f0693c378b346d7
Claudius Nascimento <claudius@claudiusnascimento.com>
2021-08-19 00:45:09 -0300

    testimonials - video

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// https://bestjquery.com/tutorial/testimonial/demo58/
+
 $(document).ready(function() {
 
     if($('a[data-rel^=lightcase]').length) {
@@ -14,5 +16,17 @@ $(document).ready(function() {
     
         $('a[data-rel^=lightcase]').lightcase(lightCaseConfig);
     }
+
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[980,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:true
+    });
     
 })
