a589e69e5693fe90d06373f9c666a581e7f8338b
Claudius Nascimento <claudius@claudiusnascimento.com>
2021-09-07 23:29:50 -0300

    images adjusts and SEO

--- send.php
+++ send.php
@@ -2,14 +2,17 @@
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['message'];
+$message = $name . ' - ' . $email . ' - ' . $_POST['message'];
+
 // O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
 // O return-path deve ser ser o mesmo e-mail do remetente.
 $headers = "MIME-Version: 1.1\r\n";
 $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
-$headers .= "From: site@centrodefalcoariafoz.com.br\r\n"; // remetente
-$headers .= "Return-Path: fabiana@centrodefalcoariafoz.com.br\r\n"; // return-path
-$envio = mail("cau@claudiusnascimento.com", "Contato enviado do Site", $message, $headers);
+$headers .= "From: contato@centrodefalcoariafoz.com.br\r\n"; // remetente
+$headers .= "Reply-To: " . $email . "\r\n" .
+$headers .= "Return-Path: ". $email ."\r\n"; // return-path
+
+$envio = mail("fabiana@centrodefalcoariafoz.com.br", "Contato enviado do Site", $message, $headers);
  
 if($envio) {
     echo json_encode(['success' => true, 'message' => 'Mensagem enviada com sucesso']);
